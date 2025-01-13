47f178099e58485d7d2adab5699f9209fe8fc2db
azhongisme <1322501114@qq.com>
2025-01-13 19:20:46 +0800

    双向认证

--- grpc/client/client.go
+++ grpc/client/client.go
@@ -2,20 +2,32 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"grpc_study/client/service"
 	"log"
+	"os"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 )
 
 func main() {
-	creds, err := credentials.NewClientTLSFromFile("../cert/server.pem", "*.zhong")
-	if err != nil {
-		log.Fatal("证书错误", err)
-	}
-	conn, err := grpc.Dial("127.0.0.1:8002", grpc.WithTransportCredentials(creds))
+	// creds, err := credentials.NewClientTLSFromFile("../cert/server.pem", "*.zhong")
+	// if err != nil {
+	// 	log.Fatal("证书错误", err)
+	// }
+	cert, _ := tls.LoadX509KeyPair("../cert/client.pem", "../cert/client.key")
+	certPoll := x509.NewCertPool()
+	ca, _ := os.ReadFile("../cert/ca.crt")
+	certPoll.AppendCertsFromPEM(ca)
+	creds := credentials.NewTLS(&tls.Config{
+		Certificates: []tls.Certificate{cert},
+		ServerName: "*.zhong",
+		RootCAs: certPoll,
+	})
+	conn, err := grpc.NewClient("127.0.0.1:8002", grpc.WithTransportCredentials(creds))
 	if err != nil {
 		log.Fatal("connect error: ", err)
 	}
--- grpc/server/server.go
+++ grpc/server/server.go
@@ -1,22 +1,42 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"grpc_study/model"
 	"grpc_study/service"
 	"log"
 	"net"
+	"os"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 )
 
 func main() {
-	cred, err := credentials.NewServerTLSFromFile("../cert/server.pem", "../cert/server.key")
+	// cred, err := credentials.NewServerTLSFromFile("../cert/server.pem", "../cert/server.key")
+	// if err != nil {
+	// 	log.Fatal("证书生成错误", err)
+	// }
+	cert, err := tls.LoadX509KeyPair("../cert/server.pem", "../cert/server.key")
 	if err != nil {
-		log.Fatal("证书生成错误", err)
+		log.Fatal("证书读取错误", err)
 	}
 
-	rpcServer := grpc.NewServer(grpc.Creds(cred))
+	certPool := x509.NewCertPool()
+	ca, err := os.ReadFile("../cert/ca.crt")
+	if err != nil {
+		log.Fatal("ca证书读取错误")
+	}
+	certPool.AppendCertsFromPEM(ca)
+
+	creds := credentials.NewTLS((&tls.Config{
+		Certificates: []tls.Certificate{cert},
+		ClientAuth: tls.RequireAndVerifyClientCert,
+		ClientCAs: certPool,
+	}))
+
+	rpcServer := grpc.NewServer(grpc.Creds(creds))
 
 	service.RegisterProductServiceServer(rpcServer, model.ProductService)
 
